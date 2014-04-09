573602599b6334916077b8e9b9754e916d096bdd
Jorge Tomé Hernando <jorge@jorgetome.info>
2014-04-09 13:40:22 +0200

    Update the name of the app

--- src/console.php
+++ src/console.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
-$console = new Application('Silex - Kitchen Edition', '0.1');
+$console = new Application('SMART Search - Livenews. Search view', '0.1');
 
 $app->boot();
 
