b04bce28c19f9bf766821f502d7bd5a1a9774068
Jorge Tomé Hernando <jorge@jorgetome.info>
2014-04-16 18:42:44 +0200

    Issue #11 - Removing from the repository external JavaScript libraries.

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -22,18 +22,16 @@ $app['assetic.enabled']              = true;
 $app['assetic.path_to_cache']        = $app['cache.path'] . '/assetic' ;
 $app['assetic.path_to_web']          = __DIR__ . '/../../web/assets';
 $app['assetic.input.path_to_assets'] = __DIR__ . '/../assets';
-
+// Style sheets
 $app['assetic.input.path_to_css']       = array(
 		$app['assetic.input.path_to_assets'] . '/css/base.css',
 		$app['assetic.input.path_to_assets'] . '/css/style.css'
 );
 $app['assetic.output.path_to_css']      = 'css/styles.css';
+// JavaScript libraries
 $app['assetic.input.path_to_js']        = array(
-    $app['assetic.input.path_to_assets'] . '/js/jquery-1.8.2.js',
     $app['assetic.input.path_to_assets'] . '/js/transition.js',
     $app['assetic.input.path_to_assets'] . '/js/funciones.js',
-    $app['assetic.input.path_to_assets'] . '/js/jquery.mousewheel.js',
-    $app['assetic.input.path_to_assets'] . '/js/jquery.jscrollpane.min.js',
     $app['assetic.input.path_to_assets'] . '/js/SimpleMediaPlayer.min.js',
 );
 $app['assetic.output.path_to_js']       = 'js/scripts.js';
