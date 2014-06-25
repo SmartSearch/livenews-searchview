6d211523b2823ab5a19d35d4e07df685592e8bee
jesusMarevalo <jesus.m.arevalo@gmail.com>
2014-06-25 09:58:10 +0000

    Solving Issue #7 - profiles, hastag and external urls of twitter opened on a new tab

--- resources/assets/js/funciones.js
+++ resources/assets/js/funciones.js
@@ -17,7 +17,7 @@ var preload = '<div id="preloader" syle="text-align:center;margin:0;"><img src="
 var noresult = '<ul class="jspPane"><li>      <div class="txt_widget"> <p class="autor"> 0 Resultados </p> <p>Esta consulta no genera resultados </p>       </div>    </li></ul>';
 var activo = "";
 var divVacio = '<div class="flexa"></div>';
-var uri = "index_dev.php/api/v1";
+var uri = "api/v1";
 
 // Solving Issue #1 - @jesusMarevalo - 20140616 - Optimize the search interface
 var init_search = false;
